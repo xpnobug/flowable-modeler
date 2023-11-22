7c2ec338bfb9af5956235a2441385fed4c52812f
曹绪培 <2877406366@qq.com>
2023-11-22 16:24:15 +0800

    bpm流程引擎

---
+++ src/main/java/com/bpm/flowablemodeler/FlowableModelerApplication.java
@@ -0,0 +1,13 @@
+package com.bpm.flowablemodeler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableModelerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowableModelerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bpm/flowablemodeler/FlowableModelerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpm.flowablemodeler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowableModelerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
